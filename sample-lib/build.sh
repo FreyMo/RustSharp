d62c925d46f9ebcf428cced7d8b8d49627860109
Moritz Freyburger <moritz@freyburger.io>
2022-07-25 20:43:35 +0200

    Fix it for macOS

--- sample-lib/build.sh
+++ sample-lib/build.sh
@@ -6,7 +6,7 @@ fi
 
 echo $release
 
-arm_targets=("aarch64-apple-darwin" "aarch64-unknown-linux-gnu") # ("aarch64-apple-darwin" "aarch64-unknown-linux-gnu" "aarch64-pc-windows-msvc")
+arm_targets=("aarch64-apple-darwin") # ("aarch64-apple-darwin" "aarch64-unknown-linux-gnu" "aarch64-pc-windows-msvc")
 x86_64_targets=("x86_64-pc-windows-gnu") # ("x86_64-apple-darwin" "x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu")
 
 for target in "${arm_targets[@]}";
